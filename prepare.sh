0b01ae1c6d33672f517f7b182ba0e5bdbccfc98e
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2020-04-03 13:37:36 +0300

    Up

--- prepare.sh
+++ prepare.sh
@@ -2,28 +2,21 @@
 
 RHEL=$(rpm -E %{rhel})
 
-if [[ ${RHEL} -eq 6 ]]; then
-  yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
-elif [[ ${RHEL} -eq 7 ]]; then
-  yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-elif [[ ${RHEL} -eq 8 ]]; then
-  yum -y install http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-fi
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-${RHEL}.rpm
 
 # Remi: PHP version older than 7.2 are not available for RHEL 8
-REPO=remi-php56
-if [[ "${RHEL}" -ge "6" ]]; then
-  REPO=remi-php72
-fi
-
-if test -f /usr/bin/dnf; then
-  dnf config-manager --set-enabled remi
-  dnf config-manager --set-enabled ${REPO}
+if [[ "${RHEL}" -ge "8" ]]; then
+  dnf -y install yum-utils
+  dnf -y module reset php
+  dnf -y module install php:remi-7.2
+  dnf -y install php-pear-phing
 else
+  REPO=remi-php56
   yum -y install yum-utils
   yum-config-manager --enable remi
   yum-config-manager --enable ${REPO}
+  # php-pear-phing in remi is no good
+  yum -y install --disablerepo=remi* php-pear-phing
 fi
 
-# php-pear-phing in remi is no good
-yum -y install --disablerepo=remi* php-pear-phing
+
