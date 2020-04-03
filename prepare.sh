f3889c464b8bc8c671f910d5fbd0bb722f528bf6
Danila Vershinin <250071+dvershinin@users.noreply.github.com>
2020-04-03 13:30:20 +0300

    Only PHP 7.2 available on RHEL 8

--- prepare.sh
+++ prepare.sh
@@ -10,13 +10,19 @@ elif [[ ${RHEL} -eq 8 ]]; then
   yum -y install http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 fi
 
+# Remi: PHP version older than 7.2 are not available for RHEL 8
+REPO=remi-php56
+if [[ "${RHEL}" -ge "6" ]]; then
+  REPO=remi-php72
+fi
+
 if test -f /usr/bin/dnf; then
   dnf config-manager --set-enabled remi
-  dnf config-manager --set-enabled remi-php56
+  dnf config-manager --set-enabled ${REPO}
 else
   yum -y install yum-utils
   yum-config-manager --enable remi
-  yum-config-manager --enable remi-php56
+  yum-config-manager --enable ${REPO}
 fi
 
 # php-pear-phing in remi is no good
