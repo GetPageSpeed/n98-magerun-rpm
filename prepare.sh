9981ef825d313f3af609690cf58bb0a79eda69a2
Danila Vershinin <ciapnz@gmail.com>
2022-09-06 02:29:13 +0300

    Up

--- prepare.sh
+++ prepare.sh
@@ -9,8 +9,6 @@ if [[ "${RHEL}" -ge "8" ]]; then
   dnf -y install yum-utils
   dnf -y module reset php
   dnf -y module install php:remi-7.2
-  # php-pear-phing deps are in "remi"
-  dnf config-manager --enable remi
 else
   REPO=remi-php56
   yum -y install yum-utils
@@ -18,6 +16,4 @@ else
   yum-config-manager --enable ${REPO}
 fi
 
-# php-pear-phing in remi is good
-yum -y install php-pear-phing
 
